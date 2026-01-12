52dc8c421ee74b0c66b4e79127e636da3bbb1724
parsilver <4928451+parsilver@users.noreply.github.com>
2026-01-12 06:05:37 +0000

    Fix styling

--- src/Commands/SchemaTablesCommand.php
+++ src/Commands/SchemaTablesCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Farzai\LaravelSchema\Commands;
 
-use Farzai\LaravelSchema\Schema\Table;
 use Farzai\LaravelSchema\SchemaInspector;
 use Illuminate\Console\Command;
 
